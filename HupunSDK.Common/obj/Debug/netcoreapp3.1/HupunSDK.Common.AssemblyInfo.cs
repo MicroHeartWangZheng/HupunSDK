d37432b0993bc3538a5feee950e64816807d0c38
王郑 <418706636@qq.com>
2020-08-02 11:30:04 +0800

    1

--- HupunSDK.Common/obj/Debug/netcoreapp3.1/HupunSDK.Common.AssemblyInfo.cs
+++ HupunSDK.Common/obj/Debug/netcoreapp3.1/HupunSDK.Common.AssemblyInfo.cs
@@ -13,11 +13,11 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("HupunSDK.Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.1")]
 [assembly: System.Reflection.AssemblyProductAttribute("HupunSDK.Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HupunSDK.Common")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.1.0")]
 
 // 由 MSBuild WriteCodeFragment 类生成。
 
